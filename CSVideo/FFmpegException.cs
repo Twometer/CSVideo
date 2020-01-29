f45a38f9b58009b01472439bc3db6da815f5bc27
Twometer <twometer@outlook.de>
2020-01-29 17:36:08 +0100

    Add library and loader

---
+++ CSVideo/FFmpegException.cs
@@ -0,0 +1,29 @@
+﻿using FFmpeg.AutoGen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSVideo
+{
+    public class FFmpegException : Exception
+    {
+        public FFmpegException(string message) : base(message)
+        {
+        }
+
+        public FFmpegException(string message, int errorCode)
+            : base($"{message}: {ErrorToString(errorCode)}")
+        {
+        }
+
+        private static unsafe string ErrorToString(int i)
+        {
+            var b = new byte[500];
+            fixed (byte* bp = b)
+                ffmpeg.av_strerror(i, bp, 500);
+            return Encoding.ASCII.GetString(b);
+        }
+    }
+}
---
+++ CSVideo/FFmpegLoader.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FFmpeg.AutoGen;
+
+namespace CSVideo
+{
+    public class FFmpegLoader
+    {
+        private static bool IsLoaded { get; set; } = false;
+
+        public static bool Load(string libraryLocation)
+        {
+            try
+            {
+                ffmpeg.RootPath = libraryLocation;
+                ffmpeg.av_version_info();
+                IsLoaded = true;
+                return true;
+            }
+            catch (DllNotFoundException)
+            {
+                return false;
+            }
+        }
+
+        internal static void EnsureLoaded()
+        {
+            if (!IsLoaded)
+            {
+                throw new FFmpegException("The FFmpeg libraries are not loaded. Please call FFmpegLoader.Load() before attempting to use ffmpeg.");
+            }
+        }
+
+    }
+}
