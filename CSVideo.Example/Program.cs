7d4a033b7da588208f3f8e67c346629ca153e563
Twometer <twometer@outlook.de>
2020-01-29 18:02:13 +0100

    Add example project

---
+++ CSVideo.Example/Program.cs
@@ -0,0 +1,30 @@
+﻿using CSVideo.Writer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSVideo.Example
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var success = FFmpegLoader.Load("");
+            if (!success)
+            {
+                Console.WriteLine("Could not load FFmpeg");
+                return;
+            }
+
+            Console.WriteLine($"Loaded FFmpeg v{FFmpegLoader.FFmpegVersion}");
+
+            using (var writer = new VideoWriter("test.mp4"))
+            {
+                // TODO
+            }
+
+        }
+    }
+}
